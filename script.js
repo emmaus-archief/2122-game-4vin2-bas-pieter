0459d86154d8d2b35164dde0e78ed62838cf909a
Basb0404 <112654@emmauscollege.nl>
2022-06-09 10:29:26 +0000

    startscherm + levels aanklikken werkt + bezig met gameover scherm

--- script.js
+++ script.js
@@ -225,9 +225,22 @@ function setup() {
 
 //gameover scherm tekenen
  var GameoverScherm = function() {
+  fill("red");
+  rect(0,0,1280,720);
   textSize(50);
   fill("white");
-  text("je bent dood :)", 200, 200);
+  text("Je zuigt", 500, 100 )
+
+  fill("white");
+  var y=220
+  while (y<=550) {
+  rect(375, y, 450, 100);
+  y+=125; }
+  fill("black");
+  text("level 1", 205, 285 );
+  text("level 2", 530, 285 );
+  text("level 3", 855, 285 );
+
 
   if(keyIsDown(KEY_ENTER)) {
     spelStatus = START;
@@ -236,14 +249,20 @@ function setup() {
 
 // startscherm tekenen
 var StartScherm = function() {
+
+  // achtergrond
   fill("red");
   rect(0,0,1280,720);
+
+  // titel spel
   textSize(50);
   fill("white");
-  text("Jump King", 500, 100 )
+  text("Jump King", 490, 100 )
+
+  // de levels
   fill("white");
   var x=175
-  while(x<=850) {
+  while (x<=850) {
   rect(x, 220, 210, 100);
   x+=325; }
   fill("black");
@@ -251,8 +270,38 @@ var StartScherm = function() {
   text("level 2", 530, 285 );
   text("level 3", 855, 285 );
  
+  // de hulpknop
   fill("white");
   rect(385,400,440,100);
+
+  // level 1 aanklikken
+
+  if(mouseIsPressed && 
+    mouseX < 385 &&
+    mouseX > 175 &&
+    mouseY < 320 &&
+    mouseY > 220 ) {
+    spelStatus = SPELEN1;
+  };
+
+  //level 2 aanklikken
+
+  if(mouseIsPressed && 
+    mouseX < 710 &&
+    mouseX > 500 &&
+    mouseY < 320 &&
+    mouseY > 220 ) {
+    spelStatus = SPELEN2;
+  };
+
+  //level 3 aanklikken
+  if(mouseIsPressed && 
+    mouseX < 1035 &&
+    mouseX > 825 &&
+    mouseY < 320 &&
+    mouseY > 220 ) {
+    spelStatus = SPELEN3;
+  };
 };
 
 /**
@@ -295,9 +344,7 @@ function draw() {
   if (spelStatus === START) { 
     //tekent startscherm
     StartScherm();
-    if(mouseIsPressed && mouseX < 100) {
-      spelStatus = SPELEN1;
-    };
+
 
   }
 }
